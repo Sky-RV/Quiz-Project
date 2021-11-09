445e7455474fcf84bf7ae8c7a1908adbbcb4dd91
Sky-RV <SkyRivera89@gmail.com>
2021-11-09 21:58:29 +0330

    Home Page
    Home page contains body and app bar,

--- Code/lib/main.dart
+++ Code/lib/main.dart
@@ -1,115 +1,326 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: _buildShrineTheme(),
+      title: 'Quiz Project',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    final String card_sub = "لور میوه های انجیر زیادی تولید می کنند که بسیاری از گونه های پرندگان ، خفاش های میوه خوار ، پستانداران و موجودات دیگر را از انها تغذیه می کند ،";
+    final String card1_title = "کوییز آنلاین";
+    final String card2_title = "خدمات ما ویژه اساتید";
+    final String card3_title = "خدمات ما ویژه دانشجویان";
+    final String btn_text = "بیشتر بخوانید";
+
+
     return Scaffold(
+
+      ////////////////////////////// APP BAR //////////////////////////////
+
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: Center(
+          child: Text(
+            'Quiz Project',
+            style: TextStyle(
+              color: Colors.white
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      ////////////////////////////// DRAWER //////////////////////////////
+
+     
+
+      ////////////////////////////// BODY //////////////////////////////
+
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+
+            SizedBox(height: 10,),
+            Card(
+              shape:
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 5,
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+
+              child: Column(
+                children: [
+
+                  Padding(  // title
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      "کوییز آنلاین",
+                      style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+
+                  Padding(  // supporting text
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      card_sub,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                  Image.network('https://placeimg.com/640/480/any', fit: BoxFit.contain,),
+
+                  ButtonBar(  // button
+                    alignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFFEE537C),
+                                Color(0xFFEB927B)
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 180,
+                            height: 35,
+                            alignment: Alignment.center,
+                            child: Text('رایگانن ثبت نام کنید', style: TextStyle(fontSize: 13, color: Colors.white),),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+
+            SizedBox(height: 10,),
+            Card(
+              shape:
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 5,
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+
+              child: Column(
+                children: [
+
+                  Padding(  // title
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      card2_title,
+                      style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+
+                  Padding(  // supporting text
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      card_sub,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                  Image.network('https://placeimg.com/640/480/any', fit: BoxFit.contain,),
+
+                  ButtonBar(  // button
+                    alignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFFEE537C),
+                                Color(0xFFEB927B)
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 180,
+                            height: 35,
+                            alignment: Alignment.center,
+                            child: Text(btn_text, style: TextStyle(fontSize: 13, color: Colors.white),),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+
+            SizedBox(height: 10,),
+            Card(
+              shape:
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 5,
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+
+              child: Column(
+                children: [
+
+                  Padding(  // title
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      card3_title,
+                      style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+
+                  Padding(  // supporting text
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      card_sub,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                  Image.network('https://placeimg.com/640/480/any', fit: BoxFit.contain,),
+
+                  ButtonBar(  // button
+                    alignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFFEE537C),
+                                Color(0xFFEB927B)
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 180,
+                            height: 35,
+                            alignment: Alignment.center,
+                            child: Text(btn_text, style: TextStyle(fontSize: 13, color: Colors.white),),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+
+            SizedBox(height: 10,),
+            // computer image
+
+            SizedBox(height: 10,),
+            // contact with us
+
+          ],
+        ),
+      )
+
     );
   }
 }
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    // colorScheme: _shrineColorScheme,
+    textTheme: _buildShrineTextTheme(base.textTheme),
+  );
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+    caption: base.caption!.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14,
+      letterSpacing: defaultLetterSpacing,
+    ),
+    button: base.button!.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 14,
+      letterSpacing: defaultLetterSpacing,
+    ),
+  )
+      .apply(
+    fontFamily: 'Rubik',
+    displayColor: Color(0xFF363671),
+    bodyColor: shrineBackgroundWhite,
+  );
+}
+
+const Color shrinePink300 = Color(0xFFEB927B);
+const Color shrinePink400 = Color(0xFFEE537C);
+
+const Color shrineBlue900 = Color(0xFF363671);
+const Color shrineBlue600 = Color(0xFF3E5196);
+
+const Color shrineErrorRed = Color(0xFFC5032B);
+
+const Color shrineSurfaceWhite = Color(0xFFFFFBFA);
+const Color shrineBackgroundWhite = Colors.white;
+
+const defaultLetterSpacing = 0.03;
