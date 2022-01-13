2d168a54cd6c4ae50f6d7d5e43d5de5bf9b38e57
Sky-RV <SkyRivera89@gmail.com>
2022-01-13 22:44:30 +0330

    Count Down Timer Class

---
+++ Code/lib/Pages/Student/Exams/CounDownTimerState.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'package:get/get.dart';
+
+class CountDownTimerState extends GetxController{
+
+  var SCount = 10;
+  late Timer _timer;
+
+  void StateTimerStart(){
+
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+
+      if(SCount > 0){
+        SCount--;
+        update();
+      }
+      else{
+        _timer.cancel();
+      }
+    }
+    );
+  }
+
+  // user can set count down seconds, from TextField
+  void setnumber(var num){
+
+    SCount = int.parse(num);
+    update();
+
+  }
+
+  // pause the timer
+  void Pause(){
+    _timer.cancel();
+    update();
+  }
+
+  // reset count value to 10
+  void reset(){
+    _timer.cancel();
+    SCount = 10 ;
+    update();
+  }
+
+}
